d0e468d7f6b55221a62cbe98353c62cec7729fba
knoxemdead101 <leighton.C.knox3@gmail.com>
2021-06-27 19:07:20 -0700

    setting up shop

--- src/Hero.java
+++ src/Hero.java
@@ -1,9 +1,8 @@
 public abstract class Hero {
     final int max = 100;
     final int min = 50;
-
     private int hitPoints;
-    //private float attack = (int) (Math.random() * )
+    private float attack;
     private float defense;
 
     public Hero() {
@@ -11,10 +10,17 @@ public abstract class Hero {
         this.defense = 0.0f;
         //this.attack = 0.0f
     }
+    public Hero (int hitPoints, float attack, float defense) {
+        this.hitPoints = hitPoints;
+        this.attack = attack;
+        this.defense = defense;
 
+    }
     public abstract int getHitPoints();
     public abstract float getAttack();
     public abstract float getDefense();
     public abstract int doAttack(float modifier);
     public abstract void takeDamage(float modifier);
+    public abstract float generate_attack(float min, float max);
+    public abstract void hero_stats();
 }
--- src/source.java
+++ src/source.java
@@ -1,9 +1,21 @@
+import java.util.Random;
 public class source {
     public static void main(String[] args) {
-        //Number generation Create a method for it?
-        int max = 10;
-        int min = 5;
-        System.out.println((int)(Math.random() * 10) + 1);
-        System.out.println((int) (Math.random() * (max - min) + 1) + min);
+
+
+
+
+
+
+
+
     }
+    //generates an attack stat per each hero type object based off of the spec
+    //parameters represent the max damage number it can be set to and the lowest it can be set to
+    public static float generate_attack(float min, float max) {
+        Random generator = new Random();
+        float stat = (float) ((generator.nextFloat() * (max - min)) + min);
+        return stat;
+     }
+     //Display stats
 }
