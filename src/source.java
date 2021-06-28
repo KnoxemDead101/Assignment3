bf32eb203f5e2898e9a5cc81eaa4ee1455820bc3
knoxemdead101 <leighton.C.knox3@gmail.com>
2021-06-27 19:08:21 -0700

    setting up shop

--- src/source.java
+++ src/source.java
@@ -12,10 +12,5 @@ public class source {
     }
     //generates an attack stat per each hero type object based off of the spec
     //parameters represent the max damage number it can be set to and the lowest it can be set to
-    public static float generate_attack(float min, float max) {
-        Random generator = new Random();
-        float stat = (float) ((generator.nextFloat() * (max - min)) + min);
-        return stat;
-     }
-     //Display stats
+
 }
